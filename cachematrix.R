25c5c3b43365995e3311cdd5f568e6f4e5f6a558
lehbr01 <lehbr01@ca.com>
2015-08-21 15:14:18 -0400

    update comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##THis function will change from numeric vector to matrix vector
 
-makeCacheMatrix <- function(x = matrix()) ##change from numeric to matrix vector
-{
+makeCacheMatrix <- function(x = matrix()) {
   
   m = NULL 
-  set = function(y) {
+  set = function(y) {  ##sets the matrix vector from whatever is passed in
     x <<- y
     m <<- NULL
   }
-  get = function() x
+  get = function() x  ## gets the matrix vecor
   setinv = function(inverse) m <<- inverse   ##function that sets inverse of matrix
   getinv = function() m ##function to get the inverse matrix
-  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  list(set=set, get=get, setinv=setinv, getinv=getinv) ##returns a list of the functions
 }
 
 
@@ -23,7 +20,6 @@ makeCacheMatrix <- function(x = matrix()) ##change from numeric to matrix vector
 
 cacheSolve <- function(x, ...) 
 {
-  
   m = x$getinv()
   
   if (!is.null(m)) #checks to see if matrix has a value cached
